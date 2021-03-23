9b65764d60e5f0b3a7ea897d28aa7e5839e68acf
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-23 17:31:53 -0300

    c# pronto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int nr1 = 0, nr2 = 0, total = 0;
+
+            Console.WriteLine("digite o primeiro numero");
+            nr1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite o segundo numero");
+            nr2 = Convert.ToInt32(Console.ReadLine());
+
+            total = nr1 + nr2;
+
+            Console.WriteLine("o total da soma é "+total);
+            Console.ReadKey();
+        }
+    }
+}
